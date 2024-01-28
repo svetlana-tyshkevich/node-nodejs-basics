380082918455e008ec2ee3571231968468949778
svetlana-tyshkevich <panfilova.ss@gmail.com>
2024-01-28 13:18:59 +0200

    feat: add hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,15 @@
+import { readFile } from 'node:fs/promises';
+import { createHash } from 'node:crypto';
+
 const calculateHash = async () => {
-    // Write your code here 
+    try {
+        const text = await readFile('src/hash/files/fileToCalculateHashFor.txt', { encoding: 'utf8' });
+        const hashString = createHash('sha256').update(text).digest('hex');
+        console.log(hashString )
+    } catch (err) {
+        console.error(err.message);
+    }
+
 };
 
 await calculateHash();
